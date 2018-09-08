aa20f8dde2f782371a25ca6232bddd101f11f87c
root <root@ip-10-1-1-86.ec2.internal>
2018-09-08 09:18:32 +0000

    mn

---
+++ Jenkins.sh
@@ -0,0 +1,20 @@
+peline {
+     agent any
+     stages {
+         stage('---clean---') {
+              step {
+                  sh "mvn clean"
+                }
+               }
+              stage('--test--') {
+                  steps {
+                     sh "mvn test"
+                     }
+                }
+              stage('--package--') {
+                  steps {
+                      sh "mvn package"
+                      }
+                }
+            }
+         }
